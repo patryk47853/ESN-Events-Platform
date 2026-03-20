551a9023d527a6dcec43e11ce839e619d1286141
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-03-20 14:22:32 +0000

    Simple docker config

---
+++ event-service/src/main/java/com/esn/event/controller/EventController.java
@@ -0,0 +1,28 @@
+package com.esn.event.controller;
+
+import com.esn.event.dto.CreateEventRequest;
+import com.esn.event.entity.Event;
+import com.esn.event.service.EventService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+
+    @PostMapping
+    public Event createEvent(@Valid @RequestBody CreateEventRequest request) {
+        return eventService.createEvent(request);
+    }
+
+    @GetMapping
+    public List<Event> getEvents() {
+        return eventService.getAllEvents();
+    }
+}
