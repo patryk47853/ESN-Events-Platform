927dd68cd3d3bbe0ac5f54bc1406871ac503f90b
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-04-20 18:53:59 +0100

    Add exception: Ticket Not Found

---
+++ ticket-service/src/main/java/com/esn/ticket/exception/TicketNotFoundException.java
@@ -0,0 +1,8 @@
+package com.esn.ticket.exception;
+
+public class TicketNotFoundException extends RuntimeException {
+
+    public TicketNotFoundException(Long ticketId) {
+        super("Ticket not found with id: " + ticketId);
+    }
+}
