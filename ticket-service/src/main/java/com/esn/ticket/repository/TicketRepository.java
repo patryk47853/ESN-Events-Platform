3f4c7d29ddb141d94549ca6c825ea210b6e066aa
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-04-22 14:28:05 +0100

    Add method: findByEventId

--- ticket-service/src/main/java/com/esn/ticket/repository/TicketRepository.java
+++ ticket-service/src/main/java/com/esn/ticket/repository/TicketRepository.java
@@ -3,5 +3,9 @@ package com.esn.ticket.repository;
 import com.esn.ticket.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    List<Ticket> findByEventId(Long eventId);
 }
