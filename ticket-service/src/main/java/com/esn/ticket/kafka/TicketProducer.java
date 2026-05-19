3b6edb94b2f66277bad10246ed90843cc9e003f3
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-05-19 10:47:48 +0100

    Add new event: Ticket Cancelled

--- ticket-service/src/main/java/com/esn/ticket/kafka/TicketProducer.java
+++ ticket-service/src/main/java/com/esn/ticket/kafka/TicketProducer.java
@@ -1,5 +1,6 @@
 package com.esn.ticket.kafka;
 
+import com.esn.ticket.event.TicketCancelledEvent;
 import com.esn.ticket.event.TicketCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,9 +10,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TicketProducer {
 
-    private final KafkaTemplate<String, TicketCreatedEvent> kafkaTemplate;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendTicketCreated(TicketCreatedEvent event) {
         kafkaTemplate.send("ticket-created-topic", event);
+    }
+
+    public void sendTicketCancelled(TicketCancelledEvent event) {
+        kafkaTemplate.send("ticket-cancelled-topic", event);
     }
 }
