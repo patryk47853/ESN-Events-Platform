90e4cf793b2783846deaa8bdb3a351c0091a1acf
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-05-20 10:39:04 +0100

    Add fields: version, createdAt, updatedAt

--- ticket-service/src/main/java/com/esn/ticket/entity/Ticket.java
+++ ticket-service/src/main/java/com/esn/ticket/entity/Ticket.java
@@ -2,6 +2,11 @@ package com.esn.ticket.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -9,6 +14,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Ticket {
 
     @Id
@@ -21,4 +27,15 @@ public class Ticket {
 
     @Enumerated(EnumType.STRING)
     private TicketStatus status;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
