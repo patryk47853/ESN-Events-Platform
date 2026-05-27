c9fcf6da9c4d650cf394c4f5dbfa6c4d677b0797
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-05-27 13:41:50 +0100

    Enable scheduling

--- ticket-service/src/main/java/com/esn/ticket/TicketServiceApplication.java
+++ ticket-service/src/main/java/com/esn/ticket/TicketServiceApplication.java
@@ -3,9 +3,11 @@ package com.esn.ticket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class TicketServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TicketServiceApplication.class, args);
