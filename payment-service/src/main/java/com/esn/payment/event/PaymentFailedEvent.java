99e65521d0ae8a9283ae9c23b9a4e1df9fd5c190
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-05-30 22:35:14 +0100

    Create event: Payment Failed

---
+++ payment-service/src/main/java/com/esn/payment/event/PaymentFailedEvent.java
@@ -0,0 +1,14 @@
+package com.esn.payment.event;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentFailedEvent {
+    private Long ticketId;
+    private Long userId;
+    private String reason;
+}
