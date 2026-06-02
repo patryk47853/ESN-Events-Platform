839b56bc5067dc9dcc86bdfaa38e36dbb6a75d98
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-06-02 20:17:52 +0100

    Add method: findByStatusAndCreatedAtBefore

--- ticket-service/src/main/java/com/esn/ticket/repository/TicketRepository.java
+++ ticket-service/src/main/java/com/esn/ticket/repository/TicketRepository.java
@@ -1,11 +1,14 @@
 package com.esn.ticket.repository;
 
 import com.esn.ticket.entity.Ticket;
+import com.esn.ticket.entity.TicketStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     List<Ticket> findByEventId(Long eventId);
+    List<Ticket> findByStatusAndCreatedAtBefore(TicketStatus status, LocalDateTime dateTime);
 }
