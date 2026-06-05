936d517534ae009ae766b284b5f83068c0be3a81
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-06-05 13:53:55 +0100

    Improve consume method

--- ticket-service/src/main/java/com/esn/ticket/kafka/PaymentConsumer.java
+++ ticket-service/src/main/java/com/esn/ticket/kafka/PaymentConsumer.java
@@ -1,22 +1,41 @@
 package com.esn.ticket.kafka;
 
+import com.esn.ticket.event.PaymentFailedEvent;
 import com.esn.ticket.event.PaymentSuccessEvent;
 import com.esn.ticket.service.TicketService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentConsumer {
 
     private final TicketService ticketService;
 
-    @KafkaListener(topics = "payment-success-topic", groupId = "ticket-group")
+    @KafkaListener(topics = "payment-success-topic", groupId = "ticket-service-group")
     public void consume(PaymentSuccessEvent event) {
+        log.info("Payment received for ticket ID: {}", event.getTicketId());
 
-        System.out.println("Payment received for ticket: " + event.getTicketId());
+        try {
+            ticketService.confirmTicket(event.getTicketId());
+            log.info("Ticket ID: {} has been successfully confirmed.", event.getTicketId());
+        } catch (Exception e) {
+            log.error("Failed to confirm ticket ID: {}", event.getTicketId(), e);
+        }
+    }
 
-        ticketService.confirmTicket(event.getTicketId());
+    @KafkaListener(topics = "payment-failed-topic", groupId = "ticket-service-group")
+    public void consumePaymentFailed(PaymentFailedEvent event) {
+        log.info("Received payment failed event for ticket ID: {}. Reason: {}", event.getTicketId(), event.getReason());
+
+        try {
+            ticketService.cancelTicket(event.getTicketId(), "PAYMENT_REJECTED_" + event.getReason());
+            log.info("Ticket ID: {} has been successfully cancelled due to failed payment.", event.getTicketId());
+        } catch (Exception e) {
+            log.error("Failed to process payment failure for ticket ID: {}", event.getTicketId(), e);
+        }
     }
 }
