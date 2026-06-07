0d949ec05d380c3fca0fd17fa4f7b74513c7f164
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-06-07 12:48:42 +0100

    Improve notification consumer

--- notification-service/src/main/java/com/esn/notification/kafka/NotificationConsumer.java
+++ notification-service/src/main/java/com/esn/notification/kafka/NotificationConsumer.java
@@ -1,19 +1,37 @@
 package com.esn.notification.kafka;
 
+import com.esn.notification.event.PaymentSuccessEvent;
 import com.esn.notification.event.TicketCreatedEvent;
+import com.esn.notification.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationConsumer {
 
-    @KafkaListener(topics = "ticket-created-topic", groupId = "notification-group")
-    public void consume(TicketCreatedEvent event) {
+    private final EmailService emailService;
 
-        System.out.println("Sending notification...");
-        System.out.println("User [" + event.getUserId() +
-                "] booked ticket for event: " + event.getEventId());
+    @KafkaListener(topics = "ticket-created-topic")
+    public void consumeTicketCreated(TicketCreatedEvent event) {
+        log.info("Notification Service received TicketCreatedEvent for ticket ID: {}", event.getTicketId());
+        try {
+            emailService.sendTicketCreatedEmail(event.getUserId(), event.getTicketId());
+        } catch (Exception e) {
+            log.error("Failed to process email for created ticket ID: {}", event.getTicketId(), e);
+        }
+    }
+
+    @KafkaListener(topics = "payment-success-topic")
+    public void consumePaymentSuccess(PaymentSuccessEvent event) {
+        log.info("Notification Service received PaymentSuccessEvent for ticket ID: {}", event.getTicketId());
+        try {
+            emailService.sendPaymentSuccessEmail(event.getUserId(), event.getTicketId());
+        } catch (Exception e) {
+            log.error("Failed to process email for successful payment of ticket ID: {}", event.getTicketId(), e);
+        }
     }
 }
