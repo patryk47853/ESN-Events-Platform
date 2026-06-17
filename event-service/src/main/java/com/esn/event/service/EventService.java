1b6e8fa7e8f3eb5bdae36b9a6977d68e7a878e79
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-06-17 14:09:33 +0100

    Add method: getFinancialReport

--- event-service/src/main/java/com/esn/event/service/EventService.java
+++ event-service/src/main/java/com/esn/event/service/EventService.java
@@ -1,6 +1,7 @@
 package com.esn.event.service;
 
 import com.esn.event.dto.CreateEventRequest;
+import com.esn.event.dto.EventFinancialReport;
 import com.esn.event.entity.Event;
 import com.esn.event.exception.EventFullException;
 import com.esn.event.exception.EventNotFoundException;
@@ -72,5 +73,22 @@ public class EventService {
             event.setBookedSeats(event.getBookedSeats() - 1);
             eventRepository.save(event);
         }
+    }
+
+    public EventFinancialReport getFinancialReport(Long eventId) {
+        Event event = eventRepository.findById(eventId)
+                .orElseThrow(() -> new EventNotFoundException(eventId));
+
+        double currentRevenue = event.getPrice() * event.getBookedSeats();
+        double potentialRevenue = event.getPrice() * event.getCapacity();
+
+        return EventFinancialReport.builder()
+                .eventId(event.getId())
+                .eventTitle(event.getTitle())
+                .ticketPrice(event.getPrice())
+                .ticketsSold(event.getBookedSeats())
+                .currentRevenue(currentRevenue)
+                .potentialRevenue(potentialRevenue)
+                .build();
     }
 }
