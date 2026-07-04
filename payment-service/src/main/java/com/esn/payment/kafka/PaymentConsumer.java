2232877e0ff93f91df70ba6979446d07f0ad5051
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-07-04 11:35:11 +0100

    add logger, comment on payment logic & handle failed payment

--- payment-service/src/main/java/com/esn/payment/kafka/PaymentConsumer.java
+++ payment-service/src/main/java/com/esn/payment/kafka/PaymentConsumer.java
@@ -1,12 +1,15 @@
 package com.esn.payment.kafka;
 
+import com.esn.payment.event.PaymentFailedEvent;
 import com.esn.payment.event.TicketCreatedEvent;
 import com.esn.payment.event.PaymentSuccessEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class PaymentConsumer {
 
@@ -15,16 +18,30 @@ public class PaymentConsumer {
     @KafkaListener(topics = "ticket-created-topic", groupId = "payment-group")
     public void consume(TicketCreatedEvent event) {
 
-        System.out.println("Processing payment for ticket: " + event.getTicketId());
+        log.info("Processing payment for ticket: {}", event.getTicketId());
 
-        // test purpose
+        // TODO:
+        // This is a temporary mock payment implementation.
+        // In production this should be replaced with a real
+        // payment provider integration (Stripe, PayPal, etc.)
         boolean paymentOk = true;
 
         if (paymentOk) {
+
             paymentProducer.sendPaymentSuccess(
                     PaymentSuccessEvent.builder()
                             .ticketId(event.getTicketId())
                             .userId(event.getUserId())
+                            .build()
+            );
+
+        } else {
+
+            paymentProducer.sendPaymentFailed(
+                    PaymentFailedEvent.builder()
+                            .ticketId(event.getTicketId())
+                            .userId(event.getUserId())
+                            .reason("PAYMENT_REJECTED")
                             .build()
             );
         }
