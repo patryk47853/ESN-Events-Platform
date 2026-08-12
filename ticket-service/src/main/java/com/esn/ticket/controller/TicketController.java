5dd18afcd164681ecfb3d4f6427146b0cf29c978
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-08-12 12:46:29 +0200

    add new endpoint, extract reused method

--- ticket-service/src/main/java/com/esn/ticket/controller/TicketController.java
+++ ticket-service/src/main/java/com/esn/ticket/controller/TicketController.java
@@ -7,6 +7,7 @@ import com.esn.ticket.service.TicketService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,8 +20,11 @@ public class TicketController {
     private final TicketService ticketService;
 
     @PostMapping
-    public Ticket createTicket(@Valid @RequestBody CreateTicketRequest request) {
-        return ticketService.createTicket(request);
+    public Ticket createTicket(@RequestBody @Valid CreateTicketRequest request, JwtAuthenticationToken authentication
+    ) {
+        Long userId = extractUserId(authentication);
+
+        return ticketService.createTicket(request, userId);
     }
 
     @GetMapping("/by-event/{eventId}")
@@ -31,5 +35,22 @@ public class TicketController {
     @PutMapping("/validate")
     public ResponseEntity<TicketValidationResponse> validateTicket(@RequestParam String token) {
         return ResponseEntity.ok(ticketService.validateTicket(token));
+    }
+
+    @GetMapping("/my")
+    public List<Ticket> getMyTickets(JwtAuthenticationToken authentication) {
+        Long userId = extractUserId(authentication);
+
+        return ticketService.getTicketsForUser(userId);
+    }
+
+    private Long extractUserId(JwtAuthenticationToken authentication) {
+        Number userIdClaim = authentication.getToken().getClaim("userId");
+
+        if (userIdClaim == null) {
+            throw new IllegalStateException("JWT does not contain userId");
+        }
+
+        return userIdClaim.longValue();
     }
 }
