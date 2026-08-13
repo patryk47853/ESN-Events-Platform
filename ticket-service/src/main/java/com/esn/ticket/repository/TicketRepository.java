24f7fa26359f01ac78efa2e26ac84b2549d3007b
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-08-13 12:07:09 +0200

    add new methods: check if event exists using user id and status + find all tickets by user id ordered by create time desc

--- ticket-service/src/main/java/com/esn/ticket/repository/TicketRepository.java
+++ ticket-service/src/main/java/com/esn/ticket/repository/TicketRepository.java
@@ -5,6 +5,7 @@ import com.esn.ticket.entity.TicketStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,4 +14,6 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findByEventId(Long eventId);
     List<Ticket> findByStatusAndCreatedAtBefore(TicketStatus status, LocalDateTime dateTime);
     Optional<Ticket> findByTicketToken(String ticketToken);
+    List<Ticket> findAllByUserIdOrderByCreatedAtDesc(Long userId);
+    boolean existsByEventIdAndUserIdAndStatusIn(Long eventId, Long userId, Collection<TicketStatus> statuses);
 }
