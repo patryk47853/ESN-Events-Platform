9ccb47636e19a0368f0498775ce2bd8761c3b9bf
Patryk <109095669+patryk47853@users.noreply.github.com>
2026-08-13 11:52:20 +0200

    add basic sql data for testing

---
+++ event-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO event (title, description, location, type, date, capacity, booked_seats, price, is_free, version)
+VALUES ('Kubernetes Workshop', 'Learn Kubernetes fundamentals', 'Wroclaw', 'EDUCATIONAL', '2026-08-15 10:00:00', 50, 0, 99.99, false, 0);
+
+INSERT INTO event (title, description, location, type, date, capacity, booked_seats, price, is_free, version)
+VALUES ('ESN City Trip', 'Weekend city adventure', 'Prague', 'TRIP', '2026-09-01 08:00:00', 30, 0, 199.99, false, 0);
+
+INSERT INTO event (title, description, location, type, date, capacity, booked_seats, price, is_free, version)
+VALUES ('International Student Meetup', 'Networking and social event', 'Krakow', 'SOCIAL', '2026-08-20 18:00:00', 100, 0, 0, true, 0);
